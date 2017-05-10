ae661fbf0bf9249225d3ee3627af3d47080f8d41
= <alexininternet@gmail.com>
2017-05-11 02:16:18 +0300

    Finsh Review Section

--- Prolog.pl
+++ Prolog.pl
@@ -36,7 +36,9 @@ http:location(files, '/assets', []).
 :- http_handler(root(welcome), katigorima_parousiasi_apotelesmaton, []).
 :- http_handler(root(selection),selection,[]).
 :- http_handler(root(review),assessment,[]).
-:- http_handler(root(reviewResult),results,[]).
+:- http_handler(root(results),results,[]).
+:- http_handler(root(complete),assesment_end,[]).
+:- http_handler(root(practice),practise,[]).
 
 /*  Το κατηγόρημα checkUserPwd(UserId, PWD) είναι αληθές  εαν  
 UserId είναι το όνομα χρήστης και PWD είναι ο κωδικός χρήστη.
@@ -107,6 +109,20 @@ selection(_Request) :-
 	   [title('Πρόβλημα :-(')],
 	   [h1('Ουπς! Παρουσιάστηκε κάποιο πρόβλημα!'),
 	    a(href='/logIn','Δοκίμασε να ξανασυνδεθείς!')]).
+		
+practice(_Request) :-
+		user_id(UserId),
+		format('Content-type: text/html~n~n'),
+		current_output(Out),
+		st_render_file(views/practiceSelection, _{
+			title: 'Prolog Intelligent Tutoring Systems'
+		}, Out, []).
+
+practice(_Request) :-
+	reply_html_page(
+	   [title('Πρόβλημα :-(')],
+	   [h1('Ουπς! Παρουσιάστηκε κάποιο πρόβλημα!'),
+	    a(href='/logIn','Δοκίμασε να ξανασυνδεθείς!')]).
 
 lessons(UserId,Lesson,LessId,LessonType) :- 
         ((clause(student(UserId,PersDetails),Body1),UserType = old);
@@ -156,9 +172,44 @@ assessment(Request) :-
 			questionB: Text_exB
 			}, Out, []).
 
-results(_Request):-
-	user_id(UserId),
-	wellDone("Μπράβο ολοκλήρωσες την Αξιολόγηση!!!").
+results(Request):-
+	less_id(LessId),
+	ass_id(AssId),
+	http_parameters(Request,[ans1(Ans1,[]),ans2(Ans2,[])]),
+	clause(kb_lesson_assessment(LessId,AssId,TestId),B1),
+	bagof(AnswerId,kb_test_assessment(TestId,ExId,ExersiceType,AnswerId,Grade)^clause(kb_test_assessment(TestId,ExId,ExersiceType,AnswerId,Grade),B4), L),
+	L=[H|G],
+	G=[D|T],
+	clause(kb_answer_assessment(H,Text_ans1),B7),
+	clause(kb_answer_assessment(D,Text_ans2),B6),
+	bagof(Grade,kb_test_assessment(TestId,ExId,ExersiceType,AnswerId,Grade)^clause(kb_test_assessment(TestId,ExId,ExersiceType,AnswerId,Grade),B5), O),
+	O=[Grade1|Grad],
+	Grad=[Grade2|GrTail],
+	 ((Ans1=Text_ans1,New_InitGrade1 is Grade1);New_InitGrade1 is 0),
+	  ((Ans2=Text_ans2,New_InitGrade2 is Grade2);New_InitGrade2 is 0),
+	 New_InitGrade is New_InitGrade2 + New_InitGrade1,
+	 http_session_retractall(total_score(X)),
+	 http_session_assert(total_score(New_InitGrade)),
+	format('Content-type: text/html~n~n'),
+			current_output(Out),
+			st_render_file(views/results, _{
+			title: 'Prolog Intelligent Tutoring Systems',
+			score1: New_InitGrade1,
+			score2: New_InitGrade2,
+			totalScore: New_InitGrade,
+			ans1: Text_ans1,
+			ans2: Text_ans2
+			}, Out, []).
+			
+assesment_end(Request):-
+	total_score(TotalScore),
+	format('Content-type: text/html~n~n'),
+			current_output(Out),
+			st_render_file(views/complete, _{
+			title: 'Prolog Intelligent Tutoring Systems',
+			totalScore: TotalScore
+			}, Out, []).
+	
 	
 wellDone(Message):-
 			format('Content-type: text/html~n~n'),
@@ -172,6 +223,7 @@ logMessage(Message):-
 		http_session_retractall(less_id(X)),
 		http_session_retractall(ass_id(X)),
 		http_session_retractall(user_id(X)),
+		http_session_retractall(total_score(X)),
 		format('Content-type: text/html~n~n'),
 		current_output(Out),
 		st_render_file(views/logIn, _{
@@ -188,6 +240,9 @@ less_id(LessId) :-
 
 ass_id(AssId) :-
 	http_session_data(ass_id(AssId)),!.
+	
+total_score(TotalScore):-
+	http_session_data(total_score(TotalScore)),!.
 
 last1(X, [X]).
 last1(X, [ Head | Tail] ):- last1(X,Tail).
